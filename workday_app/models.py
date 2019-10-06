ab71d58228a832720a98545480c517ed169d2ce9
OSX <osx@MacBook-Pro-de-OSX.local>
2019-10-06 20:32:41 -0300

    modified .gitignore, add model workday and migrate

--- workday_app/models.py
+++ workday_app/models.py
@@ -4,3 +4,9 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class workday(models.Model):
+    name = models.CharField(max_length=20)
+    code = models.CharField(max_length=3)
+    schedule = models.TextField()
+    
