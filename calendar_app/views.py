d7f4ead42475533978b2c90480e51ca439cdeda9
OSX <osx@MacBook-Pro-de-OSX.local>
2019-10-14 03:02:47 -0300

    add new app calendar

--- calendar_app/views.py
+++ calendar_app/views.py
@@ -2,3 +2,6 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+
+def home(request):
+    return render(request, 'calendar_app/home.html')
--- workday/urls.py
+++ workday/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/v1.0/', include('workday_app.urls')),
+    url(r'^calendar/', include('calendar_app.urls')),
 ]
